1daab7b7c5d9b664adf3c0ba8fba6384bdad4007
Shiroe <aggelosteck@gmail.com>
2024-02-18 21:12:44 +0200

    player and ghost updates

--- src/ghost.gd
+++ src/ghost.gd
@@ -7,7 +7,8 @@ extends CharacterBody2D
 
 @onready var pathFollow: PathFollow2D = $".."
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D;
-@onready var player: CharacterBody2D = $"../../../CharacterBody2D";
+@onready var player: CharacterBody2D = $"../../../Player";
+#@onready var ray: RayCast2D = $RayCast2D;
 
 var lastPatrolPosition;
 var hasReturnedToLastPatrolPosition = true;
@@ -41,6 +42,8 @@ func _process(delta):
 
 
 func _physics_process(delta):
+	
+	
 	if not isChasing:
 		if hasReturnedToLastPatrolPosition:
 			pathFollow.progress += delta * speed
@@ -50,7 +53,7 @@ func _physics_process(delta):
 	else:
 		hasReturnedToLastPatrolPosition = false;
 		var direction = (player.global_position - global_position).normalized();
-		velocity = direction * chaseSpeed;
+		velocity = direction * lerp(speed, chaseSpeed, 0.8);
 	move_and_slide();
 		
 func moveBackToPatrol():
@@ -61,3 +64,29 @@ func moveBackToPatrol():
 		else:
 			var direction = global_position.direction_to(lastPatrolPosition).normalized()
 			velocity = direction * speed;
+
+
+func detectPlayer():
+	# TODO: fix position checking, values are wrong
+	var distance = global_position.distance_to(player.global_position);
+	print('Distance to player', distance);
+	if distance <= 64:
+		isChasing = true;
+	if distance >= 73:
+		isChasing = false;
+	
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		isChasing = true;
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		isChasing = false;
+
+
+func _on_hit_area_area_entered(area):
+	if area.name == "FlashLightArea":
+		print('hit by flashlight')
--- src/player.gd
+++ src/player.gd
@@ -14,10 +14,22 @@ extends CharacterBody2D
 @export var isLanternOn: bool = false;
 @export var isFlashLightOn: bool = false;
 
+@onready var FlashLightArea: Area2D = $Marker2D/FlashLight/FlashLightArea;
+
+@onready var ray1: RayCast2D = $Marker2D/FlashRayCast1;
+@onready var ray2: RayCast2D = $Marker2D/FlashRayCast2;
+@onready var ray3: RayCast2D = $Marker2D/FlashRayCast3;
+@onready var ray4: RayCast2D = $Marker2D/FlashRayCast4;
+@onready var ray5: RayCast2D = $Marker2D/FlashRayCast5;
+
+var flashConeAngle: float = 45.0;
+var ray_count: int = 5
+
 
 func _physics_process(delta):
 	handleInput();
-		
+
+
 	if Input.is_action_just_pressed("mouse_right_click"):
 		lantern.enabled = !lantern.enabled;
 		lanternOuter.enabled = !lanternOuter.enabled;
@@ -31,9 +43,59 @@ func _physics_process(delta):
 	
 func doFlash():
 	if not flashLight.enabled and flashLightGCD.is_stopped():
+		isFlashLightOn = true;
+		#FlashLightArea.monitorable = true;
+		#var collidingNode = doRayCastingMagic();
+		
 		flashMarker.rotation = get_angle_to(get_global_mouse_position())
 		flashLightGCD.start(0.5);
 		flashLightAnimation.play("flash", -1, 1);
+		flashLightAnimation.animation_finished.connect(
+			func(): 
+				isFlashLightOn = false;
+				FlashLightArea.monitorable = false;
+		)
+
+
+#func doRayCastingMagic():
+	#ray1.force_raycast_update();
+	#ray2.force_raycast_update();
+	#ray3.force_raycast_update();
+	#ray4.force_raycast_update();
+	#ray5.force_raycast_update();
+	#
+	#var coll1 = ray1.get_collider();
+	#var coll2 = ray2.get_collider();
+	#var coll3 = ray3.get_collider();
+	#var coll4 = ray4.get_collider();
+	#var coll5 = ray5.get_collider();
+#
+	#if ray1.is_colliding() or ray2.is_colliding() or ray3.is_colliding() or ray4.is_colliding() or ray5.is_colliding():
+		#coll1 = ray1.get_collider();
+		#coll2 = ray2.get_collider();
+		#coll3 = ray3.get_collider();
+		#coll4 = ray4.get_collider();
+		#coll5 = ray5.get_collider();
+		#
+	#if coll1 and coll1.name == "Ghost":
+		#print('coll-1 found ghost');
+		#return coll1
+		#
+	#if coll2 and coll2.name == "Ghost":
+		#print('coll-2 found ghost');
+		#return coll2;
+		#
+	#if coll3 and coll3.name == "Ghost":
+		#print('coll-3 found ghost');
+		#return coll3;
+	#
+	#if coll4 and coll4.name == "Ghost":
+		#print('coll-4 found ghost')
+		#return coll4;
+	#
+	#if coll5 and coll5.name == "Ghost":
+		#print('coll-5 found ghost');
+		#return coll5;
 
 
 func handleInput():
@@ -48,9 +110,7 @@ func updateAnimation():
 		
 	if velocity.x < 0: animation.flip_h = true;
 	if velocity.x > 0: animation.flip_h = false;
-	
-	
-	
+
 
 
 
