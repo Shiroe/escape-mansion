acf92204528d185dfe581d91b3b843788049ec2b
stsiakal <spiros.tsiakalos1@gmail.com>
2024-02-19 15:26:04 +0200

    fixed door collisions

--- src/test_level.gd
+++ src/test_level.gd
@@ -1,21 +1,16 @@
 extends Node2D
 
-@onready var final_door: Sprite2D = $finalDoor
 @onready var player: CharacterBody2D = $Player
 @onready var lever_2: Sprite2D = $lever2
 @onready var lever_3: Sprite2D = $lever3
 @onready var key_door: Sprite2D = $keyDoor
+@onready var animation_player: AnimationPlayer = $StaticBody2D3/AnimationPlayer
+var animation_played = false
 
-func _ready() -> void:
-	pass
-	
-	
 func _process(delta: float) -> void:
-	if Game.isKeyFound:
-		final_door.rotation = 0
-	if Game.lever3Found and Game.lever2Found:
-		key_door.rotation = 180
-
+	if Game.lever2Found && Game.lever3Found && !animation_played:
+		animation_player.play('open')
+		animation_played = true
 
 func _on_level_2_area_body_entered(body: Node2D) -> void:
 	if body == player:
@@ -27,5 +22,3 @@ func _on_level_3_area_body_entered(body: Node2D) -> void:
 	if body == player:
 		lever_3.flip_h = true
 		Game.lever3Found = true
-
-
