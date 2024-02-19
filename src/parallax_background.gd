d502ba8ac333dda13e96982a31b2ff56dbaf0732
Shiroe <aggelosteck@gmail.com>
2024-02-19 19:18:57 +0200

    fix: removed old bg node from main menu

--- src/parallax_background.gd
+++ src/parallax_background.gd
@@ -1,14 +1,8 @@
 extends ParallaxBackground
 
 @export var scrolling_speed = 50
-@onready var mansionBG: Sprite2D = $Mansion;
+#@onready var mansionBG: Sprite2D = $Mansion;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	scroll_offset.x -= scrolling_speed * delta
-	var viewportWidth = get_viewport().size.x
-	var viewportHeight = get_viewport().size.y
-	var scale = viewportWidth / mansionBG.texture.get_size().x
-
-	mansionBG.set_position(Vector2(viewportWidth/2, viewportHeight/2));
-	#mansionBG.set_scale(Vector2(scale, scale));
