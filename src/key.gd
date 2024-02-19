f8726ee5423d8872d1ffda4acd935d7639caed84
stsiakal <spiros.tsiakalos1@gmail.com>
2024-02-19 15:26:04 +0200

    fixed door collisions

--- src/key.gd
+++ src/key.gd
@@ -1,8 +1,10 @@
 extends Sprite2D
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var animation_player: AnimationPlayer = $"../StaticBody2D/AnimationPlayer"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == player:
 		Game.isKeyFound = true
+		animation_player.play('open')
 		queue_free()
--- src/lever1.gd
+++ src/lever1.gd
@@ -2,15 +2,14 @@ extends Sprite2D
 
 @onready var player: CharacterBody2D = $"../Player"
 @onready var lever_door: Sprite2D = $"../leverDoor"
+@onready var animation_player: AnimationPlayer = $"../StaticBody2D2/AnimationPlayer"
 var isTurned = false
 
-func _process(delta: float) -> void:
-	if isTurned:
-		lever_door.rotation = 180.0
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if not isTurned:
 		if body == player:
 			self.flip_h = true
 			isTurned = true
+			animation_player.play('open')
 		
