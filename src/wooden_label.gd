596e73be6d6f0307837e8624fd5539c6a6ea5c1d
Shiroe <aggelosteck@gmail.com>
2024-02-22 03:08:25 +0200

    ui styles update and scene instanciation adjustment for wooden labels

--- src/wooden_label.gd
+++ src/wooden_label.gd
@@ -1,5 +1,11 @@
 extends Sprite2D
-@onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var canvas_layer: CanvasLayer = $CanvasLayer;
+@onready var label: Label = $CanvasLayer/NinePatchRect/Label;
+
+@export_multiline var LabelText = '';
+
+func _ready():
+	label.text = LabelText;
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
